5c24d34aa8251595cc52f4f803173d1764db6c0d
dushayevdev <baxtiyorddushaev@gmail.com>
2025-01-09 23:32:50 +0500

    fix mistake of link

---
+++ assets/SearchPage-BGeAJrZ8.js
@@ -0,0 +1,2 @@
+import{_ as e,c,o as r}from"./index-zzjWAnh7.js";const o={};function t(a,n){return r(),c("div")}const _=e(o,[["render",t]]);export{_ as default};
+//# sourceMappingURL=SearchPage-BGeAJrZ8.js.map
---
+++ assets/TvsPage-BiymQS1S.js
@@ -0,0 +1,2 @@
+import{_ as e,c,o}from"./index-zzjWAnh7.js";const r={};function t(n,s){return o(),c("div")}const _=e(r,[["render",t]]);export{_ as default};
+//# sourceMappingURL=TvsPage-BiymQS1S.js.map
